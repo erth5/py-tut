2fc7b3ec50a529123ea7ee58af7b95fb9aef970e
Rthem@gmx.de <ma51adof>
2020-12-22 21:59:13 +0100

    from tut morph

---
+++ file_read.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+    F = open('file', 'r+')
+    print(f.read()
--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-# Strings
-v = [2, 4, 5]
-print(v[0])
+output = 'hello world, "Maskierung": I\'m'
+v = [2, 4, "meow"]
+en = 0
+de = 1
+fr = 2
+ch = 4
+while v[0] > 0:
+    v[0] = v[0] - 1
+    print(v[0])
 
-de = 5
+for w in v:
+    print(2**3 == 8)    # potenz
 
+for i in range(5, 8, 2):
+    v.append("hi")
+    print(i)
 
-def modu(de):
-    while de > 2:
-        de = de-1
-    return de
-
-
-ex = modu(de)
-print(ex)
+# errors
+all = de / 0
